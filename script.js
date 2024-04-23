68ce0c7c820b82f92b403a0bd08a0ab598e77dfa
lorenam3 <lorenamustac03@gmail.com>
2024-04-23 22:30:50 +0200

    Added Swiper slider to display various fitness classes within the `classes-container` section.

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function showMenu() {
 function hideMenu() {
   navLinks.style.right = "-200px";
 }
+
+/* classes pricing*/
+
+let swiperClasses = new Swiper(".classes-container", {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
